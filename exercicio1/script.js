9c0e47977e3a3e521177d27ea0af7a3cd8450625
PedroHenriqueLira <pedrohenriquecardoso99@hotmail.com>
2022-08-09 20:28:18 -0300

    Exercicio ifs aninhados e ifs ternarios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,30 @@
-const num = //seu numero aqui
+// Codigo de Murilo
+// const num = 15;
+// if(num){
+//     console.log(num % 2 === 0? 'O número é divisivel por 2' : '')
+//     if(num % 3 === 0) {
+//       console.log('O número é divisivel por 3')
+//     }
+//   }
 
+
+
+
+// ifs aninhados
+const num = +prompt('Digite seu numero aqui.')
+
+if (num % 2 === 0){
+    console.log('seu numero é divisivel por 2')
+
+    if (num % 3 === 0){
+    console.log('seu numero é divisivel por 3')
+
+    }else{ console.log('O numero não é divisivel por 2')
+}
+}else{ console.log("o numero não é divisivel por 3")}
+
+
+
+// utilizando operador lógico
+
+if (num % 2 === 0 || num % 3 === 0){console.log('esse numero é divisivel por 2 e por 3')}
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,35 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
-}
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
+
+
+switch (nacionalidade){
+    case "brasileira":
+        console.log("você é do brasil")
+        break
+    case "argentina":
+        console.log("você é da argentina")
+        break
+    case "uruguaia":
+        console.log("você é do uruguai")
+        break
+    case "chilena":
+        console.log("você é do chile")
+        break
+    case "colombiana":
+        console.log("você é da colombia")
+        break
+    default:
+        console.log("você não é de nenhum desses paises") }
